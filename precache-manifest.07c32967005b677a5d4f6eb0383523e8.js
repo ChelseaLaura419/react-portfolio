2cbdfba8aeee0bb373783fa3e608b2239a11599b
Chelsea Laura <chelsealaura@Chelseas-MacBook-Pro.local>
2019-05-14 19:47:38 -0400

    Updates

---
+++ precache-manifest.07c32967005b677a5d4f6eb0383523e8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d6bdfcf711be4b6d24738b23b86df8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "43e6348072dbcf4368fc",
+    "url": "/react-portfolio/static/css/main.afa16834.chunk.css"
+  },
+  {
+    "revision": "b7e52e13af59bab34405",
+    "url": "/react-portfolio/static/js/2.5a47f11e.chunk.js"
+  },
+  {
+    "revision": "43e6348072dbcf4368fc",
+    "url": "/react-portfolio/static/js/main.190dc5c1.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "c9bf88266f84f236837d0426b7590271",
+    "url": "/react-portfolio/static/media/bamazon.c9bf8826.jpg"
+  },
+  {
+    "revision": "c8248280f89eda0620688b170895c45e",
+    "url": "/react-portfolio/static/media/band.c8248280.jpg"
+  },
+  {
+    "revision": "67cceb6d5d0da499d59838cf96522906",
+    "url": "/react-portfolio/static/media/bathhouse.67cceb6d.jpg"
+  },
+  {
+    "revision": "1047138f7fa006b3f88ec09ac30c5d52",
+    "url": "/react-portfolio/static/media/burger.1047138f.png"
+  },
+  {
+    "revision": "5d3f98f2a720cdda964e96fe765ca969",
+    "url": "/react-portfolio/static/media/combat.5d3f98f2.png"
+  },
+  {
+    "revision": "90fb4b4935d887c6f0613ecaea38ddea",
+    "url": "/react-portfolio/static/media/cwallenquest.90fb4b49.png"
+  },
+  {
+    "revision": "aa819f966c4973f04778b7e65a54c19b",
+    "url": "/react-portfolio/static/media/jaws.aa819f96.jpg"
+  },
+  {
+    "revision": "399cd905d870e3a16bd091c480221c6b",
+    "url": "/react-portfolio/static/media/liri.399cd905.jpg"
+  },
+  {
+    "revision": "15b88a9f21d76941b56e99afbaf26416",
+    "url": "/react-portfolio/static/media/nhl.15b88a9f.png"
+  },
+  {
+    "revision": "b52a82984086e7680e1189e22274bea8",
+    "url": "/react-portfolio/static/media/patriot.b52a8298.png"
+  },
+  {
+    "revision": "fa5cc4b24a3e11a988e16665257cbfe3",
+    "url": "/react-portfolio/static/media/rupaul.fa5cc4b2.jpg"
+  },
+  {
+    "revision": "9accba74e9b25df07494736ee672a1bb",
+    "url": "/react-portfolio/static/media/superjail.9accba74.jpg"
+  },
+  {
+    "revision": "db0b1f90fc0ee93a2d0fd13f44e780d4",
+    "url": "/react-portfolio/static/media/trains.db0b1f90.jpg"
+  }
+]);
